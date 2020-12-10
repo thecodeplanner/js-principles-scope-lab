73efdb569be5cd5407caf345b11ce379f6b53c4d
thecodeplanner <thecodeplanner@gmail.com>
2020-12-09 20:22:20 -0500

    commit tree 54f914cd5e9a5ed1b63a0f2fe42d8c935ab19a30

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+var bestCustomer
+const leastFavoriteCustomer = 'not sure'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+     bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'another bob'
+}
